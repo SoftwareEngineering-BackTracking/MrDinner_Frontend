3e00283fe52558dccc4cc9b36328415e549945bb
dlawotjr1999 <dlawotjr1999@gmail.com>
2022-11-11 00:01:21 +0900

    tmpCommit

--- UI_Design/js/userManagement.js
+++ UI_Design/js/userManagement.js
@@ -1,7 +1,7 @@
 var url = "https://e308edc5-f1f5-4191-942d-9173192644d7.mock.pstmn.io";
 
-function fetchAllStyle() {
-  fetch(url + "/api/style", {
+function fetchUser() {
+  fetch(url + "/api/user", {
     method: "GET",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
@@ -19,8 +19,8 @@ function fetchAllStyle() {
     .catch((error) => console.log("error", error));
 }
 
-function fetchAllStyleIngredient() {
-  fetch(url + "/api/styleingredinet", {
+function fetchAllDinner() {
+  fetch(url + "/api/dinner", {
     method: "GET",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
@@ -34,6 +34,7 @@ function fetchAllStyleIngredient() {
 
       var tempRes = JSON.stringify(response);
       var resData = JSON.parse(tempRes);
+
     })
     .catch((error) => console.log("error", error));
 }
