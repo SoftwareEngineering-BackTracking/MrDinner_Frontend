44af59f574ab16930accee42790c21f997af51af
dlawotjr1999 <dlawotjr1999@naver.com>
2022-11-10 15:47:24 +0900

    유저관리 페이지 완성

---
+++ UI_Design/js/stockManagement.js
@@ -0,0 +1,39 @@
+var url = "https://e308edc5-f1f5-4191-942d-9173192644d7.mock.pstmn.io";
+
+function fetchAllStyle() {
+  fetch(url + "/api/style", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+
+      var tempRes = JSON.stringify(response);
+      var resData = JSON.parse(tempRes);
+    })
+    .catch((error) => console.log("error", error));
+}
+
+function fetchAllStyleIngredient() {
+  fetch(url + "/api/styleingredinet", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+
+      var tempRes = JSON.stringify(response);
+      var resData = JSON.parse(tempRes);
+    })
+    .catch((error) => console.log("error", error));
+}
