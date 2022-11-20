327137e665f7e3ae4f07d908d95e5e996f440831
김민서 <ms9648@naver.com>
2022-11-20 23:56:36 +0900

    Update menuInfo.js

--- UI_Design/js/menuInfo.js
+++ UI_Design/js/menuInfo.js
@@ -1,12 +1,17 @@
 var url = "https://e308edc5-f1f5-4191-942d-9173192644d7.mock.pstmn.io";
 
-function fetchDinner() {
-  fetch(url + "/api/dinner", {
-    method: "GET",
+
+function createCartItem() {
+  var style = document.getElementById('style-select');
+  fetch(url + "/api/cartitem", {
+    method: "POST",
     headers: {
-      "Content-Type": "application/json;charset=utf-8",
-      'dinner': 1
+      "Content-Type": "application/json;charset=utf-8"
     },
+    body: JSON.stringify({
+      dinner: document.getElementById('Password').value,
+      style: style.options[style.selectedIndex].value
+    })
   })
     .then((response) => {
       return response.json();
