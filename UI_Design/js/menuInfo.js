5df9a0a21da5ae2694ca3a85807cc743dd25886f
김민서 <ms9648@naver.com>
2022-11-23 15:02:26 +0900

    Update menuInfo.js

--- UI_Design/js/menuInfo.js
+++ UI_Design/js/menuInfo.js
@@ -1,24 +1,90 @@
-var url = "https://e308edc5-f1f5-4191-942d-9173192644d7.mock.pstmn.io";
+document.write('<script src="../js/cookie.js"></script>');
 
+var url = "http://ec2-15-164-24-71.ap-northeast-2.compute.amazonaws.com:8080";
 
-function createCartItem() {
-  var style = document.getElementById('style-select');
-  fetch(url + "/api/cartitem", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8"
-    },
-    body: JSON.stringify({
-      dinner: document.getElementById('Password').value,
-      style: style.options[style.selectedIndex].value
-    })
+const login = async () => {
+  const postResponse = await fetch(url+"/api/auth/login", {
+      mode: 'cors',
+      method: "GET",
+      headers: {
+      'Content-Type':'application/json;charset=utf-8',
+      'Access-Control-Allow-Origin':'*',
+      'Connection': 'keep-alive',
+      'Accept': '*/*',
+      'id': getCookie('id'),
+      'password': getCookie('password')
+      }
   })
-    .then((response) => {
+  //const post = await postResponse.json()
+  .then((response) => {
+      if (response.ok){
+          return console.log(response.json());
+      }}).then((response)=> {
+        localStorage.setItem('access-token', response.value);
+      }).catch((error) => {
+          console.log('로그인 실패');
+      })
+};
+
+
+if (document.getElementsByClassName('dinner-name')[0].textContent == 'Valentine Dinner'){
+  var dinner = '발렌타인';
+} else if(document.getElementsByClassName('dinner-name')[0].textContent == 'English Dinner'){
+  var dinner = '잉글리시';
+} else if(document.getElementsByClassName('dinner-name')[0].textContent == 'French Dinner'){
+  var dinner = '프렌치';
+} else{
+  var dinner = '샴페인';
+};
+
+const createCartItem = async () => {
+  var style = document.getElementById('style-select');
+  data = {
+    'dinner': dinner,
+    'style': style.options[style.selectedIndex].value
+  };
+  const postResponse = await fetch(url + "/api/cartitem", {
+    method: "POST",
+    mode: 'cors',
+    headers: {
+    'Content-Type':'application/json;charset=utf-8',
+    'Access-Control-Allow-Origin':'*',
+    Connection: 'keep-alive',
+    Accept: '*/*',
+    
+    },
+    body: JSON.stringify(data)
+  }).then((response) => {
       return response.json();
     })
-    .then((response) => {
-      console.log(response.dinnerList);
-
-      })
     .catch((error) => console.log("error", error));
 }
+
+// document.getElementsByClassName('add-cart-box')[0].onclick = function() {
+//   createCartItem();
+// }
+
+
+// 카트 아이템 
+const fetchCartItem = async () => {
+  const postResponse = await fetch(url+"/api/cartitem", {
+      mode: 'cors',
+      method: "GET",
+      credentials: 'same-origin',
+      headers: {
+      'Content-Type':'application/json;charset=utf-8',
+      'Access-Control-Allow-Origin':'*',
+      Connection: 'keep-alive',
+      Accept: '*/*'
+      }
+  })
+  //const post = await postResponse.json()
+  .then((response) => {
+      if (response.ok){
+          console.log("response:", response.json());
+          console.log(response.headers);
+          return console.log(document.cookie);
+      }}).catch((error) => {
+          console.log('로그인 실패');
+      })
+};
