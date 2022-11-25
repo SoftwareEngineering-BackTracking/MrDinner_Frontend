316e3124d569a198c742d09abe27181d105c6f2e
김민서 <ms9648@naver.com>
2022-11-25 17:29:03 +0900

    Update cart.js

--- UI_Design/js/cart.js
+++ UI_Design/js/cart.js
@@ -1,46 +1,51 @@
-var url = "https://e308edc5-f1f5-4191-942d-9173192644d7.mock.pstmn.io"
+var url = "http://ec2-15-164-24-71.ap-northeast-2.compute.amazonaws.com:8080";
+document.write('<script src="../js/cookie.js"></script>');
 
-const login = async () => {
-    const postResponse = await fetch(url+"/api/auth/login", {
+
+const fetchCartItem = async () => {
+    const postResponse = await fetch(url+"/api/cartitem", {
+        mode: 'cors',
         method: "GET",
         headers: {
         'Content-Type':'application/json;charset=utf-8',
-        'id': document.getElementById('ID').value,
-        'password': document.getElementById('Password').value
+        'Access-Control-Allow-Origin': '*',
+        Connection: 'keep-alive',
+        Accept: '*/*',
+        id: getCookie('id')
         }})
-    //const post = await postResponse.json()
     .then((response) => {
         
-        modalOn(); // 일단 대기창 띄워놓기
-        if (response.ok){
-            console.log("response:", response.json());
-            setCookie(document.getElementById('ID').value, document.getElementById('ID').value, 20); // 쿠키 저장
-            setTimeout(function() {
-                modalOff()}, 1000); // 성공시 1초 후 대기창 내리기
-            go_main();
-            return console.log('쿠키 저장 완료(로그인 성공)')
-        }}).catch((error) => {
-            console.log('로그인 실패');
+        response.json()
+        }).then((response) => {
+            return response
+        }).catch((error) => {
+            console.log('장바구니 아이템 불러오기 실패');
         })
     
 }
 
-window.onload = function() {
-    fetch(url+"/api/cartitem", {
-        method: "GET",
+const deleteCartItem = async (cartNo) => {
+    const postResponse = await fetch(url+"/api/cartitem", {
+        mode: 'cors',
+        method: "DEL",
         headers: {
-        'Content-Type':'application/json;charset=utf-8'
-        }}).then((response) => {
-            return response.json()
-        }).then((response) => {
-            console.log(response.cartItems.length)
-            for (var i = 0; i < response.cartItems.length; i++){
-                const dinner = response.cartItems[i].dinner;
-                const cartNo = response.cartItems[i].cartNo;
-                const price = response.cartItems[i].price;
-                const style = response.cartItems[i].style;
+        'Content-Type':'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+        Connection: 'keep-alive',
+        Accept: '*/*',
+        id: getCookie('id')
+        },
+        body:JSON.stringify({
+            'cartItemNo': cartNo
+        })}).then((response) => {
+            if (response.ok){
+                alert('장바구니 품목이 삭제되었습니다!');
             }
-            
-            
+        response.json()
+        }).then((response) => {
+            return response
+        }).catch((error) => {
+            console.log('장바구니 아이템 불러오기 실패');
         })
-}
+    
+};
