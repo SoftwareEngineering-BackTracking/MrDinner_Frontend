f54d7b7a906f22b65714e54c1c89ac89c7d9d904
김민서 <ms9648@naver.com>
2022-11-27 19:53:06 +0900

    Update cart.js

--- UI_Design/js/cart.js
+++ UI_Design/js/cart.js
@@ -22,22 +22,22 @@ const fetchCartItem = async () => {
 
             for(i=0; i<res.cartItems.length; i++){
                 cartTest.innerHTML += `
-                <div class = 'cart-box'>
-                <div class = 'cart-img-box'>
-                    <div class = 'cart-img'></div>
-                </div>
-                <div class = 'cart-info-box'>
-                    <div class = 'title'>${res.cartItems[i].dinner.dinner}</div>
-                    <div class = 'style'>${res.cartItems[i].style.style}</div>
-                    <div class = 'hash-tag'>${res.cartItems[i].dinner.detail}</div>
-                    <div class = 'hash-tag'>${res.cartItems[i].style.detail}</div>
-                </div>
-                <div class = 'cart-right-box'>
-                    <button class = 'cart-delete' id = '${i}delete-button' onclick = "deleteCartItem(${res.cartItems[i].cartItemNo})">삭제하기</button>
-                    <div class = 'cart-price'>${res.cartItems[i].price}원</div>
-                </div>
-            </div>
-        
+                    <div class = 'cart-box'>
+                        <div class = 'cart-img-box'>
+                            <div class = 'cart-img'></div>
+                        </div>
+                        <div class = 'cart-info-box'>
+                            <p id = 'cartItemNo'></p>
+                            <div class = 'title'>${res.cartItems[i].dinner.dinner}</div>
+                            <div class = 'style'>${res.cartItems[i].style.style}</div>
+                            <div class = 'style' style = 'cursor: pointer;' onclick="renderDetail(document.getElementById('title').innerText,document.getElementById('cartItemNo').innerText)">추가메뉴</div>
+                            <div class = 'hash-tag'>${res.cartItems[i].style.detail}</div>
+                        </div>
+                        <div class = 'cart-right-box'>
+                            <button class = 'cart-delete' id = '${i}delete-button' onclick = "deleteCartItem(${res.cartItems[i].cartItemNo})">삭제하기</button>
+                            <div class = 'cart-price'>${res.cartItems[i].price}원</div>
+                        </div>
+                    </div>
                 `
                 
             }
@@ -70,6 +70,186 @@ const deleteCartItem = async (cartNo) => {
             return response
         }).catch((error) => {
             console.log('장바구니 아이템 불러오기 실패');
+        })  
+};
+
+
+function renderDetail(dinner, cartNo){
+    document.getElementsByClassName('detail-modal')[0].style.display = 'flex';
+    var modalContent = document.getElementById('modal-content');
+    if (dinner == '발렌타인'){
+        modalContent.innerHTML += 
+        `
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>발렌타인_고기</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>6000원</div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    else if (dinner == '잉글리시'){
+        modalContent.innerHTML += 
+        `
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>잉글리시_고기</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>8000원</div>
+                </div>
+            </div>
+        </div>
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>샐러드_채소</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>4000원</div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    else if (dinner == '프렌치'){
+        modalContent.innerHTML += 
+        `
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>프렌치_고기</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>7000원</div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    else if (dinner == '샴페인'){
+        modalContent.innerHTML += 
+        `
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>샴페인_축제_고기</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>9000원</div>
+                </div>
+            </div>
+        </div>
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>와인</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>7000원</div>
+                </div>
+            </div>
+        </div>
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>샴페인</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>8000원</div>
+                </div>
+            </div>
+        </div>
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>계란</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>2000원</div>
+                </div>
+            </div>
+        </div>
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>빵</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>2000원</div>
+                </div>
+            </div>
+        </div>
+        <div class = 'detail-box'>       
+            <div class = 'detail-img-box'>
+                <div class = 'detail-img'></div>
+            </div>
+            <div class = 'detail-info-box'>
+                <button id = 'add-detail'>추가하기</button>
+                <div id = 'dinnerIngredient'>베이컨</div>
+                <div class = 'price-quantity-box'>
+                    <div id = 'price'>2000원</div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    else{
+        alert('세부사항 불러오기 오류!');
+    }
+}
+
+const createCartDetail = async (cartItemNo) => {
+    data = {
+        cartItemNo : cartItemNo,
+        name: [document.getElementById('dinnerIngredient').value],
+        status: ['추가'],
+        dinnerStyles: ['디너']
+        };
+    const postResponse = await fetch(url+"/api/cartdetail", {
+        method: "POST",
+        mode: 'cors',
+        headers: {
+        'Content-Type':'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin':'*',
+        Connection: 'keep-alive',
+        Accept: '*/*'
+        },
+        body: JSON.stringify(data)
+    })
+    //const post = await postResponse.json()
+    .then((response) => {
+        console.log(response.json());
+//        onInputEmail();
+        if (response.ok){
+            console.log("response:", response);
+            return console.log('메일 보내기 완료')
+        }}).catch((error) => {
+            console.log('메일 보내기 실패');
+            console.log(error);
         })
     
-};
+}
